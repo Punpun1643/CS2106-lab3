faff5e3fe1333a662a600780d8e3705ff9535df9
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-03-28 01:46:39 +0800

    Feat: 1.7

--- part1/lab3p1-shm-counter.c
+++ part1/lab3p1-shm-counter.c
@@ -29,6 +29,13 @@ int main()
     // Initialize counter
     *counter = 0;
     pid_t pid;
+    
+    // Initialize turn
+    int *turn;
+    int shmid_turn;
+    shmid_turn = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    turn = (int *) shmat(shmid_turn, NULL, 0);
+    *turn = 0;
 
     for (i = 0; i < NUM_CHILDREN; i++)
     {
@@ -43,6 +50,8 @@ int main()
     }
     else if (pid == 0)
     {
+        while (*turn != i); // do nothing until it is the process's turn
+
         // Child process
         printf("Child %d starts\n", i + 1);
         // Simulate some work
@@ -54,6 +63,10 @@ int main()
             usleep(250000);
         }
         printf("Child %d finishes with counter %d\n", i + 1, *counter);
+
+        // Processing done, increment turn to next process
+        *turn = *turn + 1;
+
         exit(EXIT_SUCCESS);
     }
 
@@ -63,7 +76,11 @@ int main()
         wait(NULL);
     }
 
-    // Print the final value of thecounter
+    // Detach turn 
+    shmdt((char *) turn);
+    shmctl(shmid_turn, IPC_RMID, 0);
+
+    // Print the final value of the counter
     printf("Final counter value: %d\n", *counter);
     
     // Detach and destroy shared memory segment
