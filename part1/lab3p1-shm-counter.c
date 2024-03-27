c132f0855452b5da4fa16ea366fa951e9953ddd2
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2024-03-27 19:20:04 +0800

    Feat: 1.4

--- part1/lab3p1-shm-counter.c
+++ part1/lab3p1-shm-counter.c
@@ -2,14 +2,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/wait.h>
 
 #define NUM_CHILDREN 5
 
 int main()
 {
+    // Create a shared memory segment
+    int shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    if (shmid < 0) {
+      perror("Cannot create shared memory!\n");
+      exit(1);
+    }
 
-    int counter = 0, i;
+    int i;
+    
+    // Attach shared memory segment to counter
+    int *counter = (int *) shmat(shmid, NULL, 0);
+    if (counter == (void *)-1) {
+      perror("Cannot attach memory!\n");
+      exit(1);
+    }
+    
+    // Initialize counter
+    *counter = 0;
     pid_t pid;
 
     for (i = 0; i < NUM_CHILDREN; i++)
@@ -30,12 +48,12 @@ int main()
         // Simulate some work
         for (int j = 0; j < 5; j++)
         {
-            counter++;
-            printf("Child %d increment counter %d\n", i + 1, counter);
+            (*counter)++;
+            printf("Child %d increment counter %d\n", i + 1, *counter);
             fflush(stdout);
             usleep(250000);
         }
-        printf("Child %d finishes with counter %d\n", i + 1, counter);
+        printf("Child %d finishes with counter %d\n", i + 1, *counter);
         exit(EXIT_SUCCESS);
     }
 
@@ -46,7 +64,10 @@ int main()
     }
 
     // Print the final value of thecounter
-    printf("Final counter value: %d\n", counter);
-
+    printf("Final counter value: %d\n", *counter);
+    
+    // Detach and destroy shared memory segment
+    shmdt((char *) counter);
+    shmctl(shmid, IPC_RMID, 0);
     return 0;
 }
